00abc0f7859f94857d8be56b1ffd3873ae9ec4d4
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-10-01 22:28:58 +0000

    Initial Commit

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+import { createClient } from 'redis';
+import { promisify } from 'utils''
+
+class RedisClient{
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+        console.log(err);
+    });
+    this.asyncGet = promisify(this.client.get).bind(this.client);
+    this.asyncSetExp = promisify(this.client.set).bind(this.client);
+    this.asyncDel = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return this.asyncGet(key);
+   }
+
+  async set(key, value, expTime) {
+       return await this.asyncSetExp(key, value, 'EX',expTime);
+  }
+
+  async del(key) {
+      return await this.asyncDel(key);
+  };
+}
+
+module.exorts = RedisClient;
