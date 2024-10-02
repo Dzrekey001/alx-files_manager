1cb98da1da256536f7a47eb740b25adcc98d5e55
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-10-02 13:45:16 +0000

    Initial Commit

---
+++ utils/db.js
@@ -0,0 +1,29 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.dbName = process.env.DB_DATABASE || 'file_Manager';
+    this.url = `mongodb://${this.host}:${this.port}`;
+    this.client = new MongoClient(this.url, { useUnifiedTopology: true });
+    this.client.connect((error) => {
+      if (!error) this.db = this.client.db(this.dbName);
+    });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments({});
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments({});
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
